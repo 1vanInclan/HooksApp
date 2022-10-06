5d21cd526b566e4c1aec80391a21d50c91deec5b
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-06 17:04:59 -0500

    creacion de hook usefetch

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import ReactDOM from 'react-dom/client'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 // import { SimpleForm } from './02.useEffect/SimpleForm'
-import { FormWithCustomHook } from './02.useEffect/FormWithCustomHook'
+// import { FormWithCustomHook } from './02.useEffect/FormWithCustomHook'
+
+import { MultipleCustomHooks } from './03-Examples/MultipleCustomHooks'
 
 // import { HooksApp } from './HooksApp'
 
@@ -14,6 +16,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <FormWithCustomHook />
+    <MultipleCustomHooks />
   /* </React.StrictMode> */
 )
