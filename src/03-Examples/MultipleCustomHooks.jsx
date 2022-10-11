f57d04fb4242b5ba89909da59994774cba0402e7
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-11 16:46:24 -0500

    Optimizaciones en el codigo 1. Codigo de barril 2. Componentes mejor organizados.

--- src/03-Examples/MultipleCustomHooks.jsx
+++ src/03-Examples/MultipleCustomHooks.jsx
@@ -1,18 +1,12 @@
-import { useCounter } from "../hooks/useCounter";
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from "../hooks";
+import { LoadingQuote, Quote } from "./";
 
 
 export const MultipleCustomHooks = () => {
 
     const {counter, increment} = useCounter(1)
-
-    // console.log(counter)
-
     const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${ counter }`);
 
-    // console.log({data, isLoading, hasError});
-
-    // Se extraen los datos de esta forma porque los datos estan en un arreglo.
 
     const {author, quote} = !!data && data[0];    
 
@@ -24,17 +18,8 @@ export const MultipleCustomHooks = () => {
 
         {
           isLoading 
-            ? (
-                <div className="alert alert-info text-center">
-                Loading...
-                </div>
-            )
-            : (
-                <blockquote className="blockquote text-end">
-                  <p className="mb-1">{quote}</p>
-                  <footer className="blockquote-footer">{author}</footer>
-                </blockquote>
-            ) 
+          ? <LoadingQuote/> 
+          : <Quote author={ author } quote={ quote }/>
         }
 
         <button className="btn btn-primary"
