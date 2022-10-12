ced14e9a26ed6e45859ab68d40bf5970d80ba712
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-12 17:10:11 -0500

    Tarea de memos

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,9 @@ import ReactDOM from 'react-dom/client'
 
 // import { Memorize } from './06-memos/Memorize'
 // import { MemoHook } from './06-memos/MemoHook'
-import { CallbackHook } from './06-memos/CallbackHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+
+import { Padre } from './07-tarea-memo/Padre'
 
 
 // import { HooksApp } from './HooksApp'
@@ -29,6 +31,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   /* </React.StrictMode> */
 )
