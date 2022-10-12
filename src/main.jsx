c7d309454506499018e60ce3f98e398f99fa5615
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-12 16:27:51 -0500

    useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
+
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
@@ -12,7 +13,9 @@ import ReactDOM from 'react-dom/client'
 
 // import { FocusScreen } from './04-useRef/FocusScreen'
 
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
+
+import { Memorize } from './06-memos/Memorize'
 
 
 // import { HooksApp } from './HooksApp'
@@ -21,6 +24,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   /* </React.StrictMode> */
 )
