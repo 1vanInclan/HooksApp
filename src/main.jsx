ce2ec4d7503324cb805a22c58d2f41462e746be2
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-18 15:52:14 -0500

    introduccion use reducer

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,8 @@ import ReactDOM from 'react-dom/client'
 
 import { Padre } from './07-tarea-memo/Padre'
 
+import './08-useReducer/intro-reducer';
+
 
 // import { HooksApp } from './HooksApp'
 
@@ -31,6 +33,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Padre />
+    // <Padre />
   /* </React.StrictMode> */
 )
