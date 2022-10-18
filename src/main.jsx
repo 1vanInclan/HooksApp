d6ed8c838503a1532ff8d28af80b2efbfb8aee4a
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-18 16:04:26 -0500

    Uso de hook UseReducer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-
-
-
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
@@ -22,9 +19,11 @@ import ReactDOM from 'react-dom/client'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
 
-import { Padre } from './07-tarea-memo/Padre'
+// import { Padre } from './07-tarea-memo/Padre'
 
-import './08-useReducer/intro-reducer';
+// import './08-useReducer/intro-reducer';
+
+import { TodoApp } from './08-useReducer/TodoApp'
 
 
 // import { HooksApp } from './HooksApp'
@@ -33,6 +32,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    // <Padre />
+     <TodoApp />
   /* </React.StrictMode> */
 )
