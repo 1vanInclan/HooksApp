37435b165728ee50b7dfe3e65c6cd710c4efeb07
Ivan Inclan <ivan.inclan.327b@gmail.com>
2022-10-20 17:09:45 -0500

    Mi version de tarea

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,48 +1,12 @@
-import { useEffect, useReducer } from "react";
+import { useTodo } from "../hooks/useTodo";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer";
 
-const initialState = []
 
-const init = () => {
-    return JSON.parse( localStorage.getItem('todos')) || [];
-}
 
 export const TodoApp = () => {
 
-    const [ todos, dispatch ] = useReducer( todoReducer, initialState, init );
-
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify( todos ));
-    }, [todos])
-    
-
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo
-        }
-
-        dispatch( action );
-    }
-
-    const handleDeleteTodo = (id) => {
-
-        // console.log({id});
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id
-        });
-    }
-
-    const handleToggleTodo = (id) => {
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id
-        });
-    }
-
+    const { todos, handleNewTodo, handleDeleteTodo, handleToggleTodo } = useTodo()
 
     return (
         <>
